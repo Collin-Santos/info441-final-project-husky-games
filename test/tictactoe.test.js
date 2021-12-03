49f1a336505396b14eefbc982d6e94db741126c3
Collin Santos <csantos3@uw.edu>
2021-12-02 18:14:22 -0800

    working tictactoe mocha tests

--- test/tictactoe.test.js
+++ test/tictactoe.test.js
@@ -17,101 +17,183 @@ describe('Tic Tac Toe - Success', () => {
             gameState: "........x",
             lastTurn: "x"
         }))
-        assert.deepEqual(tictactoe('o.......x', '........x'), {
-
-        })
-        assert.deepEqual(tictactoe('o......xx', 'o.......x'), {
-
-        })
-        assert.deepEqual(tictactoe('oo.....xx', 'o......xx'), {
-
-        })
-        assert.deepEqual(tictactoe('oo....xxx', 'oo.....xx'), {
-
-        })
+        assert.deepEqual(tictactoe('o.......x', '........x'), Object.assign({}, baseJson, {
+            gameState: "o.......x",
+            lastTurn: "o"
+        }))
+        assert.deepEqual(tictactoe('o......xx', 'o.......x'), Object.assign({}, baseJson, {
+            gameState: "o......xx",
+            lastTurn: "x"
+        }))
+        assert.deepEqual(tictactoe('oo.....xx', 'o......xx'), Object.assign({}, baseJson, {
+            gameState: "oo.....xx",
+            lastTurn: "o"
+        }))
+        assert.deepEqual(tictactoe('oo....xxx', 'oo.....xx'), Object.assign({}, baseJson, {
+            gameState: "oo....xxx",
+            lastTurn: "x",
+            gameWon: true
+        }))
     })
 
     it('Should Win - Vertical', () => {
-        assert.deepEqual(tictactoe('........x', '.........'), {
-
-        })
-        assert.deepEqual(tictactoe('o.......x', '........x'), {
-
-        })
-        assert.deepEqual(tictactoe('o....x..x', 'o....x..x'), {
-
-        })
-        assert.deepEqual(tictactoe('oo...x..x', 'o....x..x'), {
-
-        })
-        assert.deepEqual(tictactoe('oox..x..x', 'oo...x..x'), {
-
-        })
+        assert.deepEqual(tictactoe('........x', '.........'), Object.assign({}, baseJson, {
+            gameState: "........x",
+            lastTurn: "x"
+        }))
+        assert.deepEqual(tictactoe('o.......x', '........x'), Object.assign({}, baseJson, {
+            gameState: "o.......x",
+            lastTurn: "o"
+        }))
+        assert.deepEqual(tictactoe('o....x..x', 'o.......x'), Object.assign({}, baseJson, {
+            gameState: "o....x..x",
+            lastTurn: "x"
+        }))
+        assert.deepEqual(tictactoe('oo...x..x', 'o....x..x'), Object.assign({}, baseJson, {
+            gameState: "oo...x..x",
+            lastTurn: "o"
+        }))
+        assert.deepEqual(tictactoe('oox..x..x', 'oo...x..x'), Object.assign({}, baseJson, {
+            gameState: "oox..x..x",
+            lastTurn: "x",
+            gameWon: true
+        }))
     })
 
     it('Should Win - Diagonal', () => {
-        assert.deepEqual(tictactoe('......x..', '.........'), {
-
-        })
-        assert.deepEqual(tictactoe('o.....x..', '......x..'), {
-
-        })
-        assert.deepEqual(tictactoe('o...x.x..', 'o.....x..'), {
-
-        })
-        assert.deepEqual(tictactoe('oo..x.x..', 'o...x.x..'), {
-
-        })
-        assert.deepEqual(tictactoe('oox.x.x..', 'oo..x.x..'), {
-
-        })
+        assert.deepEqual(tictactoe('......x..', '.........'), Object.assign({}, baseJson, {
+            gameState: "......x..",
+            lastTurn: "x"
+        }))
+        assert.deepEqual(tictactoe('o.....x..', '......x..'), Object.assign({}, baseJson, {
+            gameState: "o.....x..",
+            lastTurn: "o"
+        }))
+        assert.deepEqual(tictactoe('o...x.x..', 'o.....x..'), Object.assign({}, baseJson, {
+            gameState: "o...x.x..",
+            lastTurn: "x"
+        }))
+        assert.deepEqual(tictactoe('oo..x.x..', 'o...x.x..'), Object.assign({}, baseJson, {
+            gameState: "oo..x.x..",
+            lastTurn: "o"
+        }))
+        assert.deepEqual(tictactoe('oox.x.x..', 'oo..x.x..'), Object.assign({}, baseJson, {
+            gameState: "oox.x.x..",
+            lastTurn: "x",
+            gameWon: true
+        }))
     })
 
     it('Should Win - P2 Wins', () => {
-        assert.deepEqual(tictactoe('........x', '.........'), {
-
-        })
-        assert.deepEqual(tictactoe('o.......x', '........x'), {
-
-        })
-        assert.deepEqual(tictactoe('o......xx', 'o.......x'), {
-
-        })
-        assert.deepEqual(tictactoe('oo.....xx', 'o......xx'), {
-
-        })
-        assert.deepEqual(tictactoe('oo...x.xx', 'oo.....xx'), {
-
-        })
-        assert.deepEqual(tictactoe('ooo..x.xx', 'oo...x.xx'), {
-
-        })
+        assert.deepEqual(tictactoe('........x', '.........'), Object.assign({}, baseJson, {
+            gameState: "........x",
+            lastTurn: "x"
+        }))
+        assert.deepEqual(tictactoe('o.......x', '........x'), Object.assign({}, baseJson, {
+            gameState: "o.......x",
+            lastTurn: "o"
+        }))
+        assert.deepEqual(tictactoe('o......xx', 'o.......x'), Object.assign({}, baseJson, {
+            gameState: "o......xx",
+            lastTurn: "x"
+        }))
+        assert.deepEqual(tictactoe('oo.....xx', 'o......xx'), Object.assign({}, baseJson, {
+            gameState: "oo.....xx",
+            lastTurn: "o"
+        }))
+        assert.deepEqual(tictactoe('oo...x.xx', 'oo.....xx'), Object.assign({}, baseJson, {
+            gameState: "oo...x.xx",
+            lastTurn: "x"
+        }))
+        assert.deepEqual(tictactoe('ooo..x.xx', 'oo...x.xx'), Object.assign({}, baseJson, {
+            gameState: "ooo..x.xx",
+            lastTurn: "o",
+            gameWon: true
+        }))
     })
 })
 
 describe('Tic Tac Toe - Tie', () => {
-    it('Should Tie', () => {
+    const baseJson = {
+        valid: true,
+        gameState: "",
+        lastTurn: "",
+        gameWon: false,
+        tied: false
+    }
 
+    it('Should Tie', () => {
+        assert.deepEqual(tictactoe('........x', '.........'), Object.assign({}, baseJson, {
+            gameState: "........x",
+            lastTurn: "x"
+        }))
+        assert.deepEqual(tictactoe('....o...x', '........x'), Object.assign({}, baseJson, {
+            gameState: "....o...x",
+            lastTurn: "o"
+        }))
+        assert.deepEqual(tictactoe('..x.o...x', '....o...x'), Object.assign({}, baseJson, {
+            gameState: "..x.o...x",
+            lastTurn: "x"
+        }))
+        assert.deepEqual(tictactoe('..x.oo..x', '..x.o...x'), Object.assign({}, baseJson, {
+            gameState: "..x.oo..x",
+            lastTurn: "o"
+        }))
+        assert.deepEqual(tictactoe('..xxoo..x', '..x.oo..x'), Object.assign({}, baseJson, {
+            gameState: "..xxoo..x",
+            lastTurn: "x"
+        }))
+        assert.deepEqual(tictactoe('..xxooo.x', '..xxoo..x'), Object.assign({}, baseJson, {
+            gameState: "..xxooo.x",
+            lastTurn: "o"
+        }))
+        assert.deepEqual(tictactoe('.xxxooo.x', '..xxooo.x'), Object.assign({}, baseJson, {
+            gameState: ".xxxooo.x",
+            lastTurn: "x"
+        }))
+        assert.deepEqual(tictactoe('oxxxooo.x', '.xxxooo.x'), Object.assign({}, baseJson, {
+            gameState: "oxxxooo.x",
+            lastTurn: "o"
+        }))
+        assert.deepEqual(tictactoe('oxxxoooxx', 'oxxxooo.x'), Object.assign({}, baseJson, {
+            gameState: "oxxxoooxx",
+            lastTurn: "x",
+            tied: true
+        }))
     })
 })
 
 describe('Tic Tac Toe - Fail', () => {
+    const baseJsonOk = {
+        valid: true,
+        gameState: "",
+        lastTurn: "x",
+        gameWon: false,
+        tied: false
+    }
+    
+    const baseJsonFail = {
+        valid: false,
+        gameState: "",
+        gameWon: false,
+        tied: false
+    }
 
     it('Should Fail - Overwrite Move', () => {
-        assert.deepEqual(tictactoe('........x', '.........'),{
-
-        })
-        assert.deepEqual(tictactoe('........o', '........x'),{
-
-        })
+        assert.deepEqual(tictactoe('........x', '.........'), Object.assign({}, baseJsonOk, {
+            gameState: "........x"
+        }))
+        assert.deepEqual(tictactoe('........o', '........x'), Object.assign({}, baseJsonFail, {
+            gameState: "........x"
+        }))
     })
 
     it('Should Fail - Double Move', () => {
-        assert.deepEqual(tictactoe('........x', '.........'), {
-
-        })
-        assert.deepEqual(tictactoe('......oox', '........x'), {
-
-        })
+        assert.deepEqual(tictactoe('........x', '.........'), Object.assign({}, baseJsonOk, {
+            gameState: "........x"
+        }))
+        assert.deepEqual(tictactoe('......oox', '........x'), Object.assign({}, baseJsonFail, {
+            gameState: "........x"
+        }))
     })
 })
