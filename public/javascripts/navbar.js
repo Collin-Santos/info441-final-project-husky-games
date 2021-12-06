a5ca71c4d4416a83ade8c1edca98bf4a1d0baaa7
Collin Santos <csantos3@uw.edu>
2021-12-06 12:37:54 -0800

    fix dropdown and .html extended url

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -1,6 +1,9 @@
 function loadNavbar() {
     document.getElementById('navbar-container').innerHTML = `
     <a class="navbar-brand" href="/">Husky Games Center</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
@@ -8,10 +11,10 @@ function loadNavbar() {
           <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item" id="nav-tic-button">
-          <a class="nav-link" href="/tictactoe.html">Tic-Tac-Toe</a>
+          <a class="nav-link" href="/tictactoe">Tic-Tac-Toe</a>
         </li>
         <li class="nav-item" id="nav-leader-button">
-          <a class="nav-link disabled" href="/leaderboards.html">Leaderboards</a>
+          <a class="nav-link" href="/leaderboards">Leaderboards</a>
         </li>
       </ul>
       <a class="btn btn-outline-success my-2 my-sm-0" href="/signin">Login</a>
