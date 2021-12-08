19302e74a37da55d326612381bbbae2c424d4f3a
Collin Santos <csantos3@uw.edu>
2021-12-07 23:43:39 -0800

    qminor update

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,18 +1,25 @@
 async function loadGreeting() {
     let userInfo = await getIdentity()
     if (userInfo.status != "error") {
-        let greetingInfo = `
+        document.getElementById("greeting-container").innerHTML = `
         <h2 id="index_greeting">Welcome Back, ${userInfo.displayname}!</h2>
-        <input type="text" id="new_username_input" />
+        <input type="text" id="new_username_input"/>
         <button onclick="buttonUpdateUsername()" id="button_update_name">Update Username</button>
         `
-        document.getElementById("greeting-container").innerHTML = greetingInfo
         document.getElementById("new_username_input").addEventListener("keyup", (e) => {
             if (e.keyCode === 13) {
                 e.preventDefault()
                 document.getElementById("button_update_name").click()
             }
         })
+
+    } else {
+        document.getElementById("greeting-container").innerHTML = `
+        <h2>Welcome Guest!</h2>
+        <p>Here is a bunch of intro text</p>
+        <p>Here is a bunch of info about benefits of signing in</p>
+        <p>Here is a call to action</p>
+        <p>Here is a button to signin through Microsoft</p>`
     }
 }
 
